b13e16329e2fe6d841154343c8b1c1373e5f9100
DeepeshKalura <deepeshkalurs@gmail.com>
2025-01-08 13:50:38 +0530

    clean the project and added shadcn button to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import {Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,16 +10,12 @@ function App() {
 
       <h1>Rovaa</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </Button>
+  
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
